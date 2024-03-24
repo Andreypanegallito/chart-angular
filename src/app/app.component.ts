5d45407243544dd667df96f51220534c52bc9257
Andrey Panegalli <andrey.panegalli@gmail.com>
2024-03-24 17:37:08 -0300

    Criado os charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,107 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chart-angular';
+  chartsBar: any = [];
+  chartsBarGroup: any = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    for (let x = 0; x < 3; x++) {
+      this.createChart(x);
+      this.createChartGroup(x);
+    }
+  }
+
+  createChart(index: any) {
+    var ctx = `chartBar${index}`;
+    var chart = new Chart(ctx, {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
+        datasets: [
+          {
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
+            stack: 'st',
+          },
+          {
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        aspectRatio: 2.5,
+      },
+    });
+    this.chartsBar.push(chart);
+  }
+  createChartGroup(index: any) {
+    var ctx = `chartBarGroup${index}`;
+    var chart = new Chart(ctx, {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
+        datasets: [
+          {
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
+          },
+          {
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+          {
+            label: '3',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+    this.chartsBarGroup.push(chart);
+  }
 }
